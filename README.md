02083319253910a5e36fb3edc3900115ef2b0c9b
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-07-01 17:47:29 +0200

    initial rails setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dateikit
+
+Dateikit makes file sharing in teams awesome. scp your files to the server and
+automatically have the files delete once they were downloaded. Can be changed
+on a per file basis. Pure awesomesauce.
+
+## Use case
+* We usually have private files we don't others except in our team to
+  download.
+* We send them a link in IRC instead of writing an email.
+* The upload should be painless, i.e. via SCP to our server.
+* The file should self destruct once downloaded by the other party.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dateikit::Application.config.secret_key_base = '6fb72b497a72b0ac0d16969e0fbe31473cf9e0d3649ab1982a6d3ca1c001bb0ed67897bbc3ffad27957b769630bac0b8ecc93c6f3ef8c983368d99b34cf98702'
